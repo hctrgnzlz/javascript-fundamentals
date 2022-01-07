a5d0f61442a2b2fdbb4293268f14f92b31b464ce
unknown <hectorgzz89@gmail.com>
2022-01-07 14:53:17 -0500

    javascript fundamentals

---
+++ assignments.js
@@ -0,0 +1,11 @@
+let country = 'Japan';
+let continent = 'Asia';
+let population = '125.8';
+
+// console.log(country);
+
+
+let isIsland = true;
+let language;
+
+console.log (isIsland)
---
+++ script.js
@@ -0,0 +1,91 @@
+/*let js = 'amazing';
+//console.log( 40 + 8 + 23 - 10);
+
+console.log('Jonas');
+
+let firstName = "Kafka";
+console.log(firstName)
+
+
+
+
+true;
+
+
+let javascriptIsFun = true;
+
+console.log(javascriptIsFun);
+
+// console.log (typeof true);
+console.log (typeof javascriptIsFun);
+console.log (typeof "Kafka");
+
+javascriptIsFun = "NO";
+console.log (typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+//dynamic typing
+year = 1999;
+console.log(typeof year);
+
+
+let age = 33;
+age = 33;
+
+const birthYear = 1989;
+// birthYear = 1990;
+
+
+
+
+
+//Math Operators
+const now = 2037;
+// const ageKafka = 2037 - 1989;
+const ageKafka = now - 1989;
+const ageSarah = 2037 - 2018;
+console.log(ageKafka, ageSarah);
+
+console.log(ageKafka * 2, ageKafka / 10, 2 ** 3);
+//2**3 means 2 to the power of 3 = 2*2*2
+
+const firstName = 'Franz';
+const lastName = 'Kafka';
+console.log(firstName + ' ' + lastName)
+
+//Assignment Operators
+let x = 10 + 5; // 15
+x += 10; //x = x + 10 = 25
+x *= 4; //x = x * 4 = 100
+x++; //x = x + 1
+x--; // x = x - 1
+console.log(x);
+
+//Comparison Operator
+console.log(ageKafka > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
+
+*/
+
+//Operator Precedence 
+const now = 2037
+const ageKafka = now - 1989;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+console.log (25 - 10 - 5);
+
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10, y = 10, x = 10 right to left
+console.log(x, y);
+
+const averageAge = (ageKafka + ageSarah) / 2;
+console.log(ageKafka, ageSarah, averageAge);
